f765d40415d32f9588a3324c0b87952ccfda1700
Tom Warner <tomwarner13@gmail.com>
2025-01-11 13:43:46 -0500

    sitewide wake js now feature complete (fite me EU)

--- assets/js/wake.js
+++ assets/js/wake.js
@@ -1,5 +1,49 @@
 const wakeDropdown = document.getElementById("wake-setting");
+let wakeLock = null;
+
+async function wakesOn() {
+  try {
+    wakeLock = await navigator.wakeLock.request("screen");
+  } catch (err) {
+    console.error(`wake lock acquisition failed: ${err.name}, ${err.message}`);
+  }
+  console.log('wake on (or silent failure)');
+  console.log(wakeLock);
+}
+
+function wakesOff() {
+  wakeLock.release().then(() => {
+    wakeLock = null;
+  });
+  window.localStorage.removeItem('_ljrWake');
+  console.log('wake off (or silent failure)');
+  console.log(wakeLock);
+}
+
+function wakesOnSitewide() {
+  window.localStorage.setItem('_ljrWake', '1');
+  wakesOn();
+}
+
+if(window.localStorage.getItem('_ljrWake') == '1') {
+  wakesOn();
+  wakeDropdown.value = 'on-sitewide';
+}
 
 wakeDropdown.addEventListener("change", function() {
-  console.log("Wake value:", this.value);
+  switch (this.value) {
+    case 'off':
+      wakesOff();
+      break;
+    case 'on-local':
+      wakesOn();
+      window.localStorage.removeItem('_ljrWake');
+      break;
+    case 'on-sitewide':
+      wakesOnSitewide();
+      break;
+    default:
+      console.log('unknown wake option ' + this.value);
+      break;
+  }
 });
